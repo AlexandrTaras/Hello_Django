0481263b2271a202f577f6a5c1c15e5b73587a69
alex2 <Alex20121990@icloud.com>
2022-08-04 11:56:33 +0300

    start file

---
+++ hello_django/hello_django/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def about(request):
+    return render(request, 'about.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def reverse(request):
+    return render(request, 'reverse.html')
